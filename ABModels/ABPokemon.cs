8753089273d52710356f8381170f7fe79879d474
Adrián Bedón <arbedon2002@gmail.com>
2023-02-01 08:44:41 -0500

    Se crea la clase para incorporar la base de datos y se configura el retorno de datos de la API

---
+++ ABModels/ABPokemon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABExamen3P.ABModels
+{
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Root
+    {
+        public int count { get; set; }
+        public object next { get; set; }
+        public object previous { get; set; }
+        public List<Pokemon> results { get; set; }
+    }
+
+}
--- ABViews/ABListadoDePokemon.xaml.cs
+++ ABViews/ABListadoDePokemon.xaml.cs
@@ -1,3 +1,6 @@
+using ABExamen3P.ABModels;
+using Newtonsoft.Json;
+
 namespace ABExamen3P.ABViews;
 
 public partial class ABListadoDePokemon : ContentPage
@@ -5,5 +8,35 @@ public partial class ABListadoDePokemon : ContentPage
 	public ABListadoDePokemon()
 	{
 		InitializeComponent();
+        LoadPokemon();
 	}
+
+    protected override void OnAppearing()
+    {
+        LoadPokemon();
+    }
+
+    private async void LoadPokemon()
+    {
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri("https://pokeapi.co/api/v2/pokemon?limit=1000");
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json");
+
+        var client =  new HttpClient();
+
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<Root>(content);
+            ABPokemonList.ItemsSource = resultado.results;
+        }
+    }
+
+    private void pokemonCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
 }
